e8217fec47f03f02a5846a0aec6476ffa315ce2a
arzuguliyev <arzuquluxan@gmail.com>
2019-03-09 12:50:24 +0400

    Urban Template

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// to close the menu
+$('#menu-close-icon').click(function()
+{
+    $('#menu-container').css('margin-left','100%');
+});
+// to open the menu
+$('#menu-icon-link').click(function()
+{
+    $('#menu-container').css('margin-left','0');
+});
